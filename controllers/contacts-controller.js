3e5ee7329b203206ca09c825654b909bad629e2a
alex12121983 <alex12121983@gmail.com>
2023-10-02 07:23:49 +0200

    Updates for routes
    added updates for restrictions on owner

--- controllers/contacts-controller.js
+++ controllers/contacts-controller.js
@@ -17,7 +17,9 @@ const getAll = async (req, res) => {
 
 const getById = async (req, res) => {
   const { id } = req.params;
-  const result = await Contact.findById(id);
+  const { _id: owner } = req.user;
+  // const result = await Contact.findById(id);
+  const result = await Contact.findOne({ _id: id, owner });
   if (!result) {
     throw HttpError(404, `Contact with id=${id} not found`);
   }
@@ -33,7 +35,13 @@ const add = async (req, res) => {
 
 const updateById = async (req, res) => {
   const { id } = req.params;
-  const result = await Contact.findByIdAndUpdate(id, req.body, { new: true });
+  const { _id: owner } = req.user;
+  // const result = await Contact.findByIdAndUpdate(id, req.body, { new: true });
+  const result = await Contact.findOneAndUpdate(
+    { _id: id, owner },
+    { $set: req.body },
+    { returnDocument: "after" }
+  );
   if (!result) {
     throw HttpError(404, `Contact with id=${id} not found`);
   }
@@ -43,7 +51,9 @@ const updateById = async (req, res) => {
 
 const deleteById = async (req, res) => {
   const { id } = req.params;
-  const result = await Contact.findByIdAndDelete(id);
+  const { _id: owner } = req.user;
+  // const result = await Contact.findByIdAndDelete(id);
+  const result = await Contact.findOneAndRemove({ _id: id, owner });
   if (!result) {
     throw HttpError(404, `Contact with id=${id} not found`);
   }
